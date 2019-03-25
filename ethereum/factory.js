56c346f114cac3ddcdca48aee91a1868c8392390
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-03-26 00:23:23 +0100

    request new and index

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xA6FD3E21Fc41Bba9665819499424b7Aa5604b45b'
+  '0x1204E1609DFE683502C89873B9024930E86Df5Ef'
   );
 
 export default instance;
